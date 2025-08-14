1dad374f96ddcfa9775b60a94e97665cada66977
USER <USER@DESKTOP-JQS98QB>
2025-08-14 09:57:45 +0530

    Added Selenium_LocatorsPractice1 class with Login, Forgot Password and Logout functionalities. Added assertions as well.

--- src/Selenium_LocatorsPractice1.java
+++ src/Selenium_LocatorsPractice1.java
@@ -3,10 +3,14 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class Selenium_LocatorsPractice1 {
 
 	public static void main(String[] args) throws InterruptedException {
+		
+		//Declaring name variable
+		String name = "Sherinminta Jose";
 		
 		//Invoking browser
 		WebDriver driver = new ChromeDriver();
@@ -65,14 +69,44 @@ public class Selenium_LocatorsPractice1 {
 		//Grab the text and print it - Parent Child tags - css selector
 		System.out.println(driver.findElement(By.cssSelector("form p")).getText());
 		
-		//Locate go to login button - 
+		//Locate go to login button - xpath and tags
+		driver.findElement(By.xpath("//div[contains(@class,'forgot')]/button[1]")).click();
+		
+		//Adding wait - Page is in transition state
+		Thread.sleep(1000);
+		
 		//Locate username - css selector - tagname#id
+		driver.findElement(By.cssSelector("input#inputUsername")).sendKeys(name);
+		
 		//Locate password - xpath - regular expression
-		//select checkbox - id locator
+		driver.findElement(By.xpath("//input[contains(@type,'pass')]")).sendKeys("rahulshettyacademy");
+		
+		//Check the checkbox - id locator
+		driver.findElement(By.id("chkboxTwo")).click();
+		
 		//Locate submit button - css selector - regular expression
+		driver.findElement(By.cssSelector("button[class*='submit']")).click();
+		
+		//Adding wait - Page is in transition state
+		Thread.sleep(1000);
+				
+		//Validate whether success message is displayed or not - tagname locator
+		System.out.println(driver.findElement(By.tagName("p")).getText());
+		
+		//Compare whether the actual and expected outputs are same
+		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
+		
+		//Locate name displayed in the page - css selector locator - tagname
+		driver.findElement(By.cssSelector("h2")).getText();
+		
+		//Check the name displayed is same as the user name entered
+		Assert.assertEquals(driver.findElement(By.cssSelector("h2")).getText(), "Hello "+name+",");
+		
+		//Locate logout button - xpath locator - text
+		driver.findElement(By.xpath("//button[text()=\"Log Out\"]")).click();
 		
 		//close the browser
-		//driver.quit();
+		driver.quit();
 		
 	}
 
